a52b9e841d4cec8ba09a246b08e72cd8497c826e
VanessaObil <117521286+VanessaObil@users.noreply.github.com>
2023-05-25 12:43:04 +0200

    Update BlueSky_querys.sql

--- BlueSky_querys.sql
+++ BlueSky_querys.sql
@@ -3,7 +3,7 @@
 -- Obtener el número de vuelos por mes y año
 SELECT MONTH(v.fecha_salida) AS mes, YEAR(v.fecha_salida) AS año, COUNT(*) AS total_vuelos
 FROM vuelo v
-GROUP BY MONTH(v.fecha_salida), YEAR(v.fecha_salida);
+GROUP BY MONTH(v.fecha_salida), YEAR(v.fecha_salida) order by year(v.fecha_salida );
 
 -- Obtener el promedio de duración de los vuelos por aerolínea
 SELECT a.nombre AS aerolinea, AVG(r.duracion) AS duracion_promedio
@@ -49,7 +49,7 @@ BEGIN
     WHERE id_avion = num_avion
    	limit 1;
     RETURN cap;
-end&& p 
+end&& 
 delimiter ;
 
 select obtener_cAapacidad_avion(4);
@@ -91,6 +91,9 @@ CREATE PROCEDURE RealizarReserva(
 BEGIN
     INSERT INTO reserva (id_reserva, id_cliente, num_vuelo, fecha_salida, fecha_reserva, estado)
     VALUES (id_reserva_param, id_cliente_param, num_vuelo_param, fecha_salida_param, fecha_reserva_param, estado_param);
+    SELECT *
+    FROM reserva r
+    where id_reserva = id_reserva_param;
 END //
 
 DELIMITER ;
@@ -116,53 +119,8 @@ DELIMITER ;
 call mostrar_capacidad_avion(4);
 
 -- *****
-DELIMITER $$
-CREATE procedure mostrar_reservas()
-BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE id_reserva INT;
-    DECLARE id_cliente INT;
-    DECLARE id_vuelo INT;
-    DECLARE fecha_reserva DATE;
-    DECLARE cursor_reservas CURSOR FOR 
-    SELECT r.id_reserva, c.id_cliente, id_vuelo, fecha_reserva 
-    FROM cliente c inner join reserva  r 
-   	on r.id_cliente = c.id_cliente inner join vuelo v 
-   	on v.num_vuelo = r.num_vuelo ;
-    
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_reservas (
-        id_reserva INT,
-        id_cliente INT,
-        id_vuelo INT,
-        fecha_reserva DATE
-    );
-    
-    OPEN cursor_reservas;
-    
-    read_loop: LOOP
-        FETCH cursor_reservas INTO id_reserva, id_cliente, id_vuelo, fecha_reserva;
-        
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-        
-        IF id_reserva IS NOT NULL then
-        SELECT CONCAT('Reserva #', id_reserva, ' realizada por el cliente #', id_cliente, ' para el vuelo #', id_vuelo, ' en la fecha ', fecha_reserva)
-    
-        END IF;
-    END LOOP;
-    
-    CLOSE cursor_reservas;
-    
-    
-    
-    DROP TEMPORARY TABLE IF EXISTS temp_reservas;
-end$$
-call mostrar_reservas; 
 
--- *****
+-- Procedimiento: Mostrar el nombre de los clientes. Uso del cursor
 
 DELIMITER &&
 
@@ -202,75 +160,6 @@ CALL MostrarNombresClientes();
 -- -----------------
 -- TRIGGERS	
 
--- Trigger: verifica la capacidad del avión antes de insertar una reserva
-
-DELIMITER &&
-
-CREATE TRIGGER verificar_capacidad_avion
-BEFORE INSERT ON reserva
-FOR EACH ROW
-BEGIN
-    DECLARE capacidad_avion INT;
-    DECLARE capacidad_actual INT;
-    
-    SELECT capacidad INTO capacidad_avion
-    FROM avion
-    WHERE num_vuelo = NEW.num_vuelo;
-    
-    SELECT COUNT(*) INTO capacidad_actual
-    FROM reserva
-    WHERE num_vuelo = NEW.num_vuelo;
-    
-    IF capacidad_actual >= capacidad_avion THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'La capacidad del avión se ha alcanzado.';
-    END IF;
-END &&
-
-DELIMITER ;
-
--- Intentar insertar una reserva en un num_vuelo con capacidad de avión alcanzada
-INSERT INTO reserva (id_reserva, id_cliente, num_vuelo, fecha_salida, fecha_reserva, estado)
-VALUES (1001, 1001, 1001, '2023-05-21 10:00:00', '2023-05-20 15:00:00', 'Pendiente');
-
--- Insertar una reserva en un num_vuelo con capacidad de avión disponible
-INSERT INTO reserva (id_reserva, id_cliente, num_vuelo, fecha_salida, fecha_reserva, estado)
-VALUES (1002, 1002, 1002, '2023-05-21 10:00:00', '2023-05-20 16:00:00', 'Pendiente');
-
-
-
--- TriggeR: Actualiza la duración del vuelo al insertar o actualizar una ruta
-DELIMITER &&
-
-CREATE TRIGGER actualizar_duracion_vuelo
-AFTER INSERT ON ruta
-FOR EACH ROW
-BEGIN
-    DECLARE duracion_vuelo TIME;
-    
-    SELECT duracion INTO duracion_vuelo
-    FROM ruta
-    WHERE num_vuelo = NEW.num_vuelo;
-    
-    UPDATE vuelo
-    SET duracion = duracion_vuelo
-    WHERE num_vuelo = NEW.num_vuelo;
-END &&
-
-DELIMITER ;
-
--- Insertar una nueva ruta con num_vuelo existente
-INSERT INTO ruta (num_vuelo, id_aerolinea, origen, destino, fecha_y_hora, duracion)
-VALUES (123, 1, 'Origen', 'Destino', '2023-05-21 10:00:00', '02:30:00');
-
--- Actualizar la duración de una ruta existente
-UPDATE ruta
-SET duracion = '03:00:00'
-WHERE num_vuelo = 123;
-
-
--- **
-
 -- Trigger en la tabla Vuelo que actualiza la 
 -- fecha de llegada automáticamente al insertar una 
 -- fecha de salida:
@@ -293,6 +182,7 @@ VALUES (1, 'En proceso', 3, '2023-05-21 10:00:00', '2023-05-21 10:00:00');
 SELECT fecha_llegada
 FROM vuelo
 WHERE num_vuelo = 1;
+
 -- Si el trigger "actualizar_fecha_llegada" está funcionando correctamente, 
 -- el resultado de la consulta debería mostrar la fecha de llegada actualizada 
 -- de acuerdo a la fecha de salida y duración proporcionadas en la inserción.
